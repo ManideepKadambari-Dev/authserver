c799346da50da897db38e80d61c4038f7a22a8fe
ManideepKadambari-Dev <manideep.kadambari.official@gmail.com>
2022-11-26 23:30:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieparser())
 app.use(cors({
     origin: process.env.origin,
     credentials: true,
-    preflightContinue : true;
+    preflightContinue : true,
 }));
 
 app.listen(PORT,()=>{
