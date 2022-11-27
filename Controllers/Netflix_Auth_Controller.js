48f23fab78b4e41b052e1d165869107973c9ae4b
ManideepKadambari-Dev <manideep.kadambari.official@gmail.com>
2022-11-27 22:24:45 +0530

    added exception handling

--- Controllers/Netflix_Auth_Controller.js
+++ Controllers/Netflix_Auth_Controller.js
@@ -7,26 +7,30 @@ const Authrouter = express.Router();
 
 //#region SignUp
 Authrouter.use("/signup", async (req, res, next) => {
-  console.log(req.headers)
-  console.log("Middleware triggered");
-  const user = await User.countDocuments({ email: req.body.email });
-  if (user === 0) next();
-  else res.status(403).send({ error: "User Already Exists" });
+  try {
+    console.log(req.headers);
+    console.log("Middleware triggered");
+    const user = await User.countDocuments({ email: req.body.email });
+    if (user === 0) next();
+    else res.status(403).send({ error: "User Already Exists" });
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 });
 
 const signupcontroller = async (req, res) => {
   console.log("Signup Requested");
   try {
-    if(!req.body.password) res.status(400).send({error:"No Password"})
-    else{
-    const hashedpassword = await bcrypt.hash(req.body.password, 10);
-    signupuser = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedpassword,
-    });
-    res.status(201).send({ message: "User Created Successfully" });
-  }
+    if (!req.body.password) res.status(400).send({ error: "No Password" });
+    else {
+      const hashedpassword = await bcrypt.hash(req.body.password, 10);
+      signupuser = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedpassword,
+      });
+      res.status(201).send({ message: "User Created Successfully" });
+    }
   } catch {
     res.status(500).send({ error: "Something went wrong" });
   }
@@ -60,53 +64,60 @@ Authrouter.use("/login", async (req, res, next) => {
       }
     } catch (err) {
       if (err.message === "invalid signature")
-        req.body.email&&req.bosy.password? res.status(401).send({ error: "Login Failed" }):next();
+        req.body.email && req.bosy.password
+          ? res.status(401).send({ error: "Login Failed" })
+          : next();
       else res.status(500).send({ message: "something went wrong" });
     }
   } else {
-    if(!req.body.email || !req.body.password) res.status(400).send({error:"Email/Password not received"})
+    if (!req.body.email || !req.body.password)
+      res.status(400).send({ error: "Email/Password not received" });
     else next();
   }
 });
 
 const logincontroller = async (req, res) => {
   console.log("Login Requested");
-  const { email, password } = req.body;
-  const loginuser = await User.findOne({ email: email });
-  if (loginuser) {
-    if (await bcrypt.compare(password, loginuser.password)) {
-      const r_token = jwt.sign(
-        {
-          id: loginuser._id,
-          username: loginuser.username,
-        },
-        process.env.R_TOKEN_SECRET,
-        { expiresIn: "5d" }
-      );
-      loginuser.rtoken = r_token;
-      await loginuser.save();
-      const a_token = jwt.sign(
-        {
-          id: loginuser._id,
-          username: loginuser.username,
-        },
-        process.env.TOKEN_SECRET,
-        { expiresIn: "1d" }
-      );
-      await res.cookie("refreshtoken", r_token, {
-        httpOnly: true,
-        domain : "manideepdev.tk",
-        max_Age: 24 * 60 * 60 * 1000 * 5,
-      });
-      res.status(200).send({
-        auth_token: a_token,
-      });
+  try {
+    const { email, password } = req.body;
+    const loginuser = await User.findOne({ email: email });
+    if (loginuser) {
+      if (await bcrypt.compare(password, loginuser.password)) {
+        const r_token = jwt.sign(
+          {
+            id: loginuser._id,
+            username: loginuser.username,
+          },
+          process.env.R_TOKEN_SECRET,
+          { expiresIn: "5d" }
+        );
+        loginuser.rtoken = r_token;
+        await loginuser.save();
+        const a_token = jwt.sign(
+          {
+            id: loginuser._id,
+            username: loginuser.username,
+          },
+          process.env.TOKEN_SECRET,
+          { expiresIn: "1d" }
+        );
+        await res.cookie("refreshtoken", r_token, {
+          httpOnly: true,
+          domain: "manideepdev.tk",
+          max_Age: 24 * 60 * 60 * 1000 * 5,
+        });
+        res.status(200).send({
+          auth_token: a_token,
+        });
+      } else {
+        res.status(401).send({ error: "Invalid Password" });
+      }
     } else {
-      res.status(401).send({ error: "Invalid Password" });
+      console.log("Not Found");
+      res.status(401).send({ error: "User Doesnot Exist" });
     }
-  } else {
-    console.log("Not Found");
-    res.status(401).send({ error: "User Doesnot Exist" });
+  } catch (e) {
+    res.status(500).send(e.message);
   }
 };
 Authrouter.post("/login", logincontroller);
@@ -114,7 +125,7 @@ Authrouter.post("/login", logincontroller);
 
 //#region Logout
 const logoutcontroller = async (req, res) => {
-  console.log(req.headers)
+  console.log(req.headers);
   const refreshtoken = req.cookies.refreshtoken;
   try {
     const verified = jwt.verify(refreshtoken, process.env.R_TOKEN_SECRET);
@@ -126,7 +137,7 @@ const logoutcontroller = async (req, res) => {
       loginuser[0].rtoken = null;
       await loginuser[0].save();
       console.log(loginuser[0].username + " logged out");
-      res.clearCookie("refreshtoken")
+      res.clearCookie("refreshtoken");
       res.status(200).send({ message: "Loogged out Successfully" });
     } else {
       res.status(401).send({ error: "Logout Failed" });
